839c2f7cf97a244da8fe5c783e4f96b64e5afd6a
Akil <akilvishnum@gmail.com>
2020-10-06 23:20:56 +0530

    Checking Phase 2

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 // akilvishnu
 // Uff. Checking again
-// checking 
+
 
 const kPrimaryColor = Color(0xFF11249F);
 var kPrimaryLightColor = Color(0xFF11249F).withOpacity(0.2);
