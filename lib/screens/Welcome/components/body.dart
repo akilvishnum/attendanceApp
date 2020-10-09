82c81480e39ffcf8a1013a6c84436afbabf562ef
Akil Vishnu <56084840+akilvishnum@users.noreply.github.com>
2020-10-09 23:23:51 +0530

    Added Welcome.svg to the file.

--- lib/screens/Welcome/components/body.dart
+++ lib/screens/Welcome/components/body.dart
@@ -29,7 +29,7 @@ class Body extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Image.asset(
-                      'assets/images/pers.png',
+                      'assets/images/welcome.png',
                       height: size.height * 0.35,
                       width: 0.5 * size.width,
                     ),
